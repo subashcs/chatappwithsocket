0d360837851856694de88338087423f7cf6e6a63
subashcs <subashsapkota59@gmail.com>
2020-01-30 20:03:05 +0545

    Updates

--- chat.js
+++ chat.js
@@ -4,23 +4,39 @@ var socket = io.connect('http://localhost:4243');
 
 
 // Query DOM
-var message = document.getElementById('message'), //message box
-      handle = document.getElementById('handle'),//handle box
-      btn = document.getElementById('send'),
+var message = document.getElementById('message'); //message box
+      handle = document.getElementById('handle');//handle box
+      btn = document.getElementById('send');
       output = document.getElementById('output');
       feedback= document.getElementById('feedback');
+      username = document.getElementById('username');
+      setuser = document.getElementById('setuser');
 
 //Emit events or handle events
+window.addEventListener('load', () => {
+    handle.style.display="none";
+  });
+
+setuser.addEventListener('click',function(){
+    if(username.value){
+        handle.style.display="block";
+        handle.innerHTML=username.value;
+        username.style.display = "none";
+        setuser.style.display="none";
+    }
+})
 
 btn.addEventListener('click',function(){
+    if(handle.innerHTML){
     socket.emit('chat',{
         message:message.value,
-        handle:handle.value
+        handle:handle.innerHTML
     })
+  }
 })
 
 message.addEventListener('keypress',function(){
-    socket.emit('typing',handle.value)
+    socket.emit('typing',handle.innerHTML)
 })
 
 //listen for events
